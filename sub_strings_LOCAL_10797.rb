4b30874662cbbe6b6f0edde01165f29e68d3ae45
mkhan503 <mahah.mujeebkhan@gmail.com>
2022-01-06 10:30:48 +0400

    Test2

--- sub_strings_LOCAL_10797.rb
+++ sub_strings_LOCAL_10797.rb
@@ -15,6 +15,6 @@ end
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 substrings("Howdy partner, sit down! How's it going?", dictionary)
-#checking
 
+#chekcing 
 
